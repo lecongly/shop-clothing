a2bdfffd92057ce7881b3be83c393592ee980266
LCL <lecongly28102002@gmail.com>
2021-11-22 20:56:55 +0700

    header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import "./App.css";
 
 import HomePage from "./pages/HomePage/HomePage";
 import ShopPage from "./pages/ShopPage/ShopPage";
-
+import Header from "./components/Header/Header";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
