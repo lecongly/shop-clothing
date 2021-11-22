1ba8811b343897f74bd3476bbd6728184512525b
LCL <lecongly28102002@gmail.com>
2021-11-22 20:35:22 +0700

    collection item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import HomePage from "./pages/HomePage/HomePage";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
+
+import HomePage from "./pages/HomePage/HomePage";
+import ShopPage from "./pages/ShopPage/ShopPage";
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
